44260b21e8c028d155f5d47c38032b5661df77c3
Kirill Ragozin <nullsfer@gmail.com>
2020-07-29 03:04:18 +0900

    Requested changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,33 +9,24 @@ $(document).ready(function() {
   */
   $(window).scroll(function() {
     if ($(window).scrollTop() > 70) {
-      // $(".navbar").removeClass("navbar-transparent");
+
     } else {
-      // $(".navbar").removeClass("navbar-translucent");
+
     }
   });
 
   // If Mobile, add background color when toggler is clicked
-  // $(".navbar-toggler").click(function() {
-  //   if (!$(".navbar-collapse").hasClass("show")) {
-  //     // $(".navbar").addClass("bg-dark");
-  //     // $(".navbar").removeClass("navbar-transparent");
-  //     // $(".navbar").addClass("bg-light");
-  //     // $(".navbar").addClass("navbar-light");
-  //     // $(".navbar").removeClass("bg-dark");
-  //     // $(".navbar").removeClass("navbar-dark");
-  //   } else {
-  //     if ($(window).scrollTop() < 70) {
-  //       // $(".navbar").removeClass("bg-dark");
-  //       // $(".navbar").addClass("navbar-transparent");
-  //       // $(".navbar").removeClass("bg-light");
-  //       // $(".navbar").removeClass("navbar-light");
-  //       // $(".navbar").addClass("bg-dark");
-  //       // $(".navbar").addClass("navbar-dark");
-  //     } else {
-  //     }
-  //   }
-  // });
+  $(".navbar-toggler").click(function() {
+    if (!$(".navbar-collapse").hasClass("show")) {
+
+    } else {
+      if ($(window).scrollTop() < 70) {
+
+      } else {
+
+      }
+    }
+  });
   // ############
 
   // document ready
